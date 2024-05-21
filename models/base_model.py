ede17c254ba45dc0b71e51cced195f3c026d3ba0
programmingwithjerry <eneyellowj@gmail.com>
2024-05-21 18:08:56 +0000

    base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -18,14 +18,18 @@ class BaseModel:
     """
 
     def __init__(self, *args, **kwargs):
-        """Initializes BaseModel with id, created_at, and updated_at attributes."""
+        """Initializes BaseModel with id, created_at,
+        and updated_at attributes
+        """
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
         if kwargs:
             for key, value in kwargs.items():
                 if key in ['created_at', 'updated_at']:
-                    setattr(self, key, datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f'))
+                    setattr(
+                        self, key,
+                        datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f'))
                 elif key != '__class__':
                     setattr(self, key, value)
         else:
@@ -36,15 +40,18 @@ class BaseModel:
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
-        """Updates the updated_at attribute with the current datetime and saves the instance."""
+        """Updates the updated_at attribute with the current datetime
+        and saves the instance.
+        """
         self.updated_at = datetime.now()
         models.storage.save()
 
     def to_dict(self):
-        """Returns a dictionary representation of the instance, including formatted datetime attributes."""
+        """Returns a dictionary representation of the instance,
+        including formatted datetime attributes.
+        """
         instance_dict = self.__dict__.copy()
         instance_dict['created_at'] = self.created_at.isoformat()
         instance_dict['updated_at'] = self.updated_at.isoformat()
         instance_dict['__class__'] = self.__class__.__name__
         return instance_dict
-
